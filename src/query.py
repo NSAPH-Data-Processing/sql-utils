d2fc9272664bd026b0e40363be3f938dd2b11bd8
Ivan Gu <zifan.gu429@gmail.com>
2022-07-05 21:36:32 -0400

    wrapper function for database connection

---
+++ src/query.py
@@ -0,0 +1,59 @@
+#  Copyright (c) 2022. Harvard University
+#
+#  Developed by Research Software Engineering,
+#  Faculty of Arts and Sciences, Research Computing (FAS RC)
+#  Authors: Michael A Bouzinier
+#           Zifan Gu
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+import json
+import sys
+from decimal import Decimal
+import pandas as pd
+
+from psycopg2.extras import RealDictCursor
+
+from nsaph.db import Connection
+
+from pandas import DataFrame
+
+######Enter your query here######
+SQL = '''
+SELECT * FROM medicaid.admissions 
+LIMIT 20
+'''
+#################################
+
+def query(db_ini_file: str, db_conn_name: str):
+    connection = Connection(db_ini_file,
+                            db_conn_name,
+                            silent=True,
+                            app_name_postfix=".sample_query")
+    
+    with connection.connect() as cnxn:
+        with cnxn.cursor(cursor_factory=RealDictCursor) as cursor:
+            print("Query in progres...")
+            cursor.execute(SQL)
+            records = cursor.fetchall()
+            print("Converting returned query to dataframe...")
+            df = pd.DataFrame([i.copy() for i in records])
+            print("Returned {:d} rows".format(len(df)))
+
+            return df
+
+
+if __name__ == '__main__':
+    df_test = query(sys.argv[1], sys.argv[2])
+    df_test.to_csv("sample_query.csv", index=False)
+                
